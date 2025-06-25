7c87ea847e2b2128e870ce8d61a2f750251d056b
Sky_Time87 <blackshadow1609@yandex.ru>
2025-06-25 05:06:34 +0300

    All buttons created

--- Calc/main.cpp
+++ Calc/main.cpp
@@ -3,6 +3,30 @@
 
 CONST CHAR g_sz_CLASS_NAME[] = "MyCalc";
 
+CONST CHAR* g_sz_OPERATIONS[] = { "+", "-", "*", "/" };
+CONST CHAR* g_sz_EDIT[] = { "<-", "C", "=" };
+
+/*CONST CHAR* g_sz_BUTTONS[4][5] =
+{
+	{"7", "8", "9", "/", "<-"}
+	{"4", "5", "6", "*", "C"}
+};*/
+
+
+//g_i_ - Global Integer;
+CONST INT g_i_BUTTON_SIZE = 50;
+CONST INT g_i_INTERVAL = 1;
+CONST INT g_i_BUTTON_SPACE = g_i_BUTTON_SIZE + g_i_INTERVAL;
+//CONST INT g_i_BUTTON_SPACE_DOUBLE = (g_i_BUTTON_SIZE + g_i_INTERVAL) * 2;
+
+CONST INT g_i_BUTTON_SIZE_DOUBLE = g_i_BUTTON_SIZE * 2 + g_i_INTERVAL;
+CONST INT g_i_START_X = 10;
+CONST INT g_i_START_Y = 10;
+CONST INT g_i_DISPLAY_HEIGHT = 22;
+CONST INT g_i_DISPLAY_WIDTH = g_i_BUTTON_SIZE * 5 + g_i_INTERVAL * 4;
+CONST INT g_i_BUTTON_START_X = g_i_START_X;
+CONST INT g_i_BUTTON_START_Y = g_i_START_Y + g_i_DISPLAY_HEIGHT + g_i_INTERVAL;
+
 INT WINAPI WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
@@ -73,13 +97,89 @@ INT WINAPI WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		(
 			NULL, "Edit", "0",
 			WS_CHILD | WS_VISIBLE | WS_BORDER | ES_RIGHT,
-			10, 10,
-			500, 22,
+			g_i_BUTTON_START_X, g_i_START_Y,
+			g_i_DISPLAY_WIDTH, g_i_DISPLAY_HEIGHT,
 			hwnd,
 			(HMENU)IDC_EDIT_DISPLAY,
 			GetModuleHandle(NULL),
 			NULL
 		);
+		INT iDigit = IDC_BUTTON_1;
+		CHAR szDigit[2] = {};
+		for (int i = 6; i >= 0; i -= 3)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				szDigit[0] = iDigit - IDC_BUTTON_0 + '0';
+				CreateWindowEx
+				(
+					NULL, "Button", szDigit,
+					WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+					g_i_BUTTON_START_X + (g_i_BUTTON_SIZE + g_i_INTERVAL) * j,
+					g_i_BUTTON_START_Y + (g_i_BUTTON_SIZE + g_i_INTERVAL) * i/3,
+					g_i_BUTTON_SIZE, g_i_BUTTON_SIZE,
+					hwnd,
+					(HMENU)iDigit,
+					GetModuleHandle(NULL),
+					NULL
+				);
+				iDigit++;
+			}
+		}
+		CreateWindowEx
+		(
+			NULL, "Button", "0",
+			WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+			g_i_BUTTON_START_X, g_i_BUTTON_START_Y + (g_i_BUTTON_SPACE) * 3,
+			g_i_BUTTON_SIZE_DOUBLE, g_i_BUTTON_SIZE,
+			hwnd,
+			(HMENU)IDC_BUTTON_0,
+			GetModuleHandle(NULL),
+			NULL
+		);
+		CreateWindowEx
+		(
+			NULL, "Button", ".",
+			WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+			g_i_BUTTON_START_X + g_i_BUTTON_SPACE * 2,
+			g_i_BUTTON_START_Y + g_i_BUTTON_SPACE * 3,
+			g_i_BUTTON_SIZE, g_i_BUTTON_SIZE,
+			hwnd,
+			(HMENU)IDC_BUTTON_POINT,
+			GetModuleHandle(NULL),
+			NULL
+		);
+		///////////////////////////////////////////////////////////////////////
+		for (int i = 0; i < 4; i++)
+		{
+			CreateWindowEx
+			(
+				NULL, "Button", g_sz_OPERATIONS[i],
+				WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+				g_i_BUTTON_START_X + g_i_BUTTON_SPACE * 3, 
+				g_i_BUTTON_START_Y + g_i_BUTTON_SPACE * (3 - i),
+				g_i_BUTTON_SIZE, g_i_BUTTON_SIZE,
+				hwnd,
+				(HMENU)IDC_BUTTON_PLUS + i,
+				GetModuleHandle(NULL),
+				NULL
+			);
+		}
+		for (int i = 0; i < 3; i++)
+		{
+			CreateWindowEx
+			(
+				NULL, "Button", g_sz_EDIT[i],
+				WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+				g_i_BUTTON_START_X + g_i_BUTTON_SPACE * 4, 
+				g_i_BUTTON_START_Y + g_i_BUTTON_SPACE * i,
+				g_i_BUTTON_SIZE, i < 2? g_i_BUTTON_SIZE: g_i_BUTTON_SIZE_DOUBLE,
+				hwnd,
+				(HMENU)(IDC_BUTTON_BSP + i),
+				GetModuleHandle(NULL),
+				NULL
+			);
+		}
 	}
 		break;
 	case WM_COMMAND:
